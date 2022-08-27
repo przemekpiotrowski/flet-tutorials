2041cf195ee4bb568c4765eba8837b300759ba81
Przemek Piotrowski <p@nic.com.pl>
2022-08-27 15:39:52 +0200

    calculator: simple calc

--- calc/calc.py
+++ calc/calc.py
@@ -6,6 +6,14 @@ def main(page: Page):
     page.title = "Calc App"
     result = Text(value="0")
 
+    def do_calc(e):
+        if e.data == "AC":
+            result.value = "0"
+        else:
+            result.value = e.data
+
+        result.update()
+
     page.add(
         Container(
             width=300,
@@ -14,41 +22,41 @@ def main(page: Page):
                     Row(controls=[result], alignment="end"),
                     Row(
                         controls=[
-                            ElevatedButton(text="AC"),
-                            ElevatedButton(text="+/-"),
-                            ElevatedButton(text="%"),
-                            ElevatedButton(text="/"),
+                            ElevatedButton(text="AC", on_click=do_calc, data="AC"),
+                            ElevatedButton(text="+/-", on_click=do_calc, data="+/-"),
+                            ElevatedButton(text="%", on_click=do_calc, data="%"),
+                            ElevatedButton(text="/", on_click=do_calc, data="/"),
                         ]
                     ),
                     Row(
                         controls=[
-                            ElevatedButton(text="7"),
-                            ElevatedButton(text="8"),
-                            ElevatedButton(text="9"),
-                            ElevatedButton(text="*"),
+                            ElevatedButton(text="7", on_click=do_calc, data="7"),
+                            ElevatedButton(text="8", on_click=do_calc, data="8"),
+                            ElevatedButton(text="9", on_click=do_calc, data="9"),
+                            ElevatedButton(text="*", on_click=do_calc, data="*"),
                         ]
                     ),
                     Row(
                         controls=[
-                            ElevatedButton(text="4"),
-                            ElevatedButton(text="5"),
-                            ElevatedButton(text="6"),
-                            ElevatedButton(text="-"),
+                            ElevatedButton(text="4", on_click=do_calc, data="4"),
+                            ElevatedButton(text="5", on_click=do_calc, data="5"),
+                            ElevatedButton(text="6", on_click=do_calc, data="6"),
+                            ElevatedButton(text="-", on_click=do_calc, data="-"),
                         ]
                     ),
                     Row(
                         controls=[
-                            ElevatedButton(text="1"),
-                            ElevatedButton(text="2"),
-                            ElevatedButton(text="3"),
-                            ElevatedButton(text="+"),
+                            ElevatedButton(text="1", on_click=do_calc, data="1"),
+                            ElevatedButton(text="2", on_click=do_calc, data="2"),
+                            ElevatedButton(text="3", on_click=do_calc, data="3"),
+                            ElevatedButton(text="+", on_click=do_calc, data="+"),
                         ]
                     ),
                     Row(
                         controls=[
-                            ElevatedButton(text="0", expand=2),
-                            ElevatedButton(text="."),
-                            ElevatedButton(text="="),
+                            ElevatedButton(text="0", on_click=do_calc, data="0", expand=2),
+                            ElevatedButton(text=".", on_click=do_calc, data="."),
+                            ElevatedButton(text="=", on_click=do_calc, data="="),
                         ]
                     ),
                 ]
