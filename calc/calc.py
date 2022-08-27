69f24523aef27e1bd0a8b7e8420c8a78f3c2b2b5
Przemek Piotrowski <p@nic.com.pl>
2022-08-27 14:24:23 +0200

    calculator: basic layout

--- calc/calc.py
+++ calc/calc.py
@@ -1,5 +1,5 @@
 import flet
-from flet import ElevatedButton, Page, Row, Text
+from flet import Column, Container, ElevatedButton, Page, Row, Text
 
 
 def main(page: Page):
@@ -7,46 +7,53 @@ def main(page: Page):
     result = Text(value="0")
 
     page.add(
-        Row(controls=[result]),
-        Row(
-            controls=[
-                ElevatedButton(text="AC"),
-                ElevatedButton(text="+/-"),
-                ElevatedButton(text="%"),
-                ElevatedButton(text="/"),
-            ]
-        ),
-        Row(
-            controls=[
-                ElevatedButton(text="7"),
-                ElevatedButton(text="8"),
-                ElevatedButton(text="9"),
-                ElevatedButton(text="*"),
-            ]
-        ),
-        Row(
-            controls=[
-                ElevatedButton(text="4"),
-                ElevatedButton(text="5"),
-                ElevatedButton(text="6"),
-                ElevatedButton(text="-"),
-            ]
-        ),
-        Row(
-            controls=[
-                ElevatedButton(text="1"),
-                ElevatedButton(text="2"),
-                ElevatedButton(text="3"),
-                ElevatedButton(text="+"),
-            ]
-        ),
-        Row(
-            controls=[
-                ElevatedButton(text="0"),
-                ElevatedButton(text="."),
-                ElevatedButton(text="="),
-            ]
-        ),
+        Container(
+            width=300,
+            content=Column(
+                controls=[
+                    Row(controls=[result], alignment="end"),
+                    Row(
+                        controls=[
+                            ElevatedButton(text="AC"),
+                            ElevatedButton(text="+/-"),
+                            ElevatedButton(text="%"),
+                            ElevatedButton(text="/"),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(text="7"),
+                            ElevatedButton(text="8"),
+                            ElevatedButton(text="9"),
+                            ElevatedButton(text="*"),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(text="4"),
+                            ElevatedButton(text="5"),
+                            ElevatedButton(text="6"),
+                            ElevatedButton(text="-"),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(text="1"),
+                            ElevatedButton(text="2"),
+                            ElevatedButton(text="3"),
+                            ElevatedButton(text="+"),
+                        ]
+                    ),
+                    Row(
+                        controls=[
+                            ElevatedButton(text="0", expand=2),
+                            ElevatedButton(text="."),
+                            ElevatedButton(text="="),
+                        ]
+                    ),
+                ]
+            ),
+        )
     )
 
 
