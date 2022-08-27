8d04aceeeefd684ce26989a8a5150d36e5d10974
Przemek Piotrowski <p@nic.com.pl>
2022-08-27 14:07:43 +0200

    calculator: rows

--- calc/calc.py
+++ calc/calc.py
@@ -1,5 +1,5 @@
 import flet
-from flet import ElevatedButton, Page, Text
+from flet import ElevatedButton, Page, Row, Text
 
 
 def main(page: Page):
@@ -7,26 +7,46 @@ def main(page: Page):
     result = Text(value="0")
 
     page.add(
-        result,
-        ElevatedButton(text="AC"),
-        ElevatedButton(text="+/-"),
-        ElevatedButton(text="%"),
-        ElevatedButton(text="/"),
-        ElevatedButton(text="7"),
-        ElevatedButton(text="8"),
-        ElevatedButton(text="9"),
-        ElevatedButton(text="*"),
-        ElevatedButton(text="4"),
-        ElevatedButton(text="5"),
-        ElevatedButton(text="6"),
-        ElevatedButton(text="-"),
-        ElevatedButton(text="1"),
-        ElevatedButton(text="2"),
-        ElevatedButton(text="3"),
-        ElevatedButton(text="+"),
-        ElevatedButton(text="0"),
-        ElevatedButton(text="."),
-        ElevatedButton(text="="),
+        Row(controls=[result]),
+        Row(
+            controls=[
+                ElevatedButton(text="AC"),
+                ElevatedButton(text="+/-"),
+                ElevatedButton(text="%"),
+                ElevatedButton(text="/"),
+            ]
+        ),
+        Row(
+            controls=[
+                ElevatedButton(text="7"),
+                ElevatedButton(text="8"),
+                ElevatedButton(text="9"),
+                ElevatedButton(text="*"),
+            ]
+        ),
+        Row(
+            controls=[
+                ElevatedButton(text="4"),
+                ElevatedButton(text="5"),
+                ElevatedButton(text="6"),
+                ElevatedButton(text="-"),
+            ]
+        ),
+        Row(
+            controls=[
+                ElevatedButton(text="1"),
+                ElevatedButton(text="2"),
+                ElevatedButton(text="3"),
+                ElevatedButton(text="+"),
+            ]
+        ),
+        Row(
+            controls=[
+                ElevatedButton(text="0"),
+                ElevatedButton(text="."),
+                ElevatedButton(text="="),
+            ]
+        ),
     )
 
 
