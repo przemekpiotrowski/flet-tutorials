0f208173707a8fd2caa5be2db258e559d606e421
Przemek Piotrowski <p@nic.com.pl>
2022-08-30 22:55:36 +0200

    c: refactor to class, allow multiple objects

--- calc/calc.py
+++ calc/calc.py
@@ -1,52 +1,51 @@
 import flet
-from flet import Column, Container, ElevatedButton, Page, Row, Text, colors
+from flet import Column, Container, ElevatedButton, Page, Row, Text, UserControl, colors
 
 BUTTON_REGULAR = 1
 BUTTON_GREY = 2
 BUTTON_ORANGE = 3
 
 
-def main(page: Page):
-    page.title = "Calc App"
-    result = Text(value=float(0), color=colors.WHITE, size=20)
+class CalculatorApp(UserControl):
+    def build(self):
+        result = Text(value=float(0), color=colors.WHITE, size=20)
 
-    def do_calc(e):
-        if e.data == "AC":
-            result.value = float(0)
+        def do_calc(e):
+            if e.data == "AC":
+                result.value = float(0)
 
-        elif e.data == "+/-":
-            result.value = 0 - float(result.value)
+            elif e.data == "+/-":
+                result.value = 0 - float(result.value)
 
-        else:
-            try:
-                result.value = float(e.data)
-            except ValueError:
-                result.value = e.data
+            else:
+                try:
+                    result.value = float(e.data)
+                except ValueError:
+                    result.value = e.data
 
-        result.update()
+            result.update()
 
-    def build_button(text, expand=1, type=BUTTON_REGULAR):
-        if type == BUTTON_GREY:
-            bgcolor = colors.BLUE_GREY_100
-            color = colors.BLACK
-        elif type == BUTTON_ORANGE:
-            bgcolor = colors.ORANGE
-            color = colors.WHITE
-        else:  # BUTTON_REGULAR
-            bgcolor = colors.WHITE24
-            color = colors.WHITE
+        def build_button(text, expand=1, type=BUTTON_REGULAR):
+            if type == BUTTON_GREY:
+                bgcolor = colors.BLUE_GREY_100
+                color = colors.BLACK
+            elif type == BUTTON_ORANGE:
+                bgcolor = colors.ORANGE
+                color = colors.WHITE
+            else:  # BUTTON_REGULAR
+                bgcolor = colors.WHITE24
+                color = colors.WHITE
 
-        return ElevatedButton(
-            text=text,
-            bgcolor=bgcolor,
-            color=color,
-            expand=expand,
-            on_click=do_calc,
-            data=text,
-        )
+            return ElevatedButton(
+                text=text,
+                bgcolor=bgcolor,
+                color=color,
+                expand=expand,
+                on_click=do_calc,
+                data=text,
+            )
 
-    page.add(
-        Container(
+        return Container(
             width=300,
             bgcolor=colors.BLACK,
             padding=20,
@@ -95,7 +94,13 @@ def main(page: Page):
                 ]
             ),
         )
-    )
+
+
+def main(page: Page):
+    page.title = "Calc App"
+
+    page.add(CalculatorApp())
+    # page.add(CalculatorApp())
 
 
 flet.app(target=main)
